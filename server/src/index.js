ad50517627747e494c022da1d2e107a3b19bf585
ariakhayer <ariakh110@gmail.com>
2021-11-07 11:27:26 +0330

    v-2 initial & conf project

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,16 @@
 import express from 'express';
+const app = express();
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    // todo
+});
+
+
+app.listen(3000,() => {
+    console.log('listen on port 3000');
+});
