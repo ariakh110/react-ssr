593a377a211a45a6a6967550f132753405e26df4
ariakhayer <ariakh110@gmail.com>
2021-11-08 22:19:28 +0330

    v-13 use magic of webpack nod externals

--- server/webpack.server.js
+++ server/webpack.server.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const baseConfig = require("./webpack.base.js");
 const { merge } = require("webpack-merge");
+const webpackNodeExternals = require("webpack-node-externals");
 
 const config = {
   mode: "none",
@@ -10,5 +11,6 @@ const config = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "build"),
   },
+  externals: [webpackNodeExternals()],
 };
 module.exports = merge(baseConfig, config);
